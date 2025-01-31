17f826cca31fe0e3b5632cd30c3c0710307ee8d3
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-01-31 23:38:06 +0100

    testing namespaces

--- src/Payment/type/cash.php
+++ src/Payment/type/cash.php
@@ -3,8 +3,8 @@ declare(strict_types= 1);
 namespace \pay\type;
 
 class Cash{
-    public function cash(float $amount, string $devis){
-        return $amount . ;
+    public function myCash(float $amount, string $devis){
+        return $amount . $devis ;
     }
 } 
 
--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,19 @@
 <?php
 
 declare(strict_types=1);
-<<<<<<< HEAD
+
 require_once '../Transaction.php';
 require_once 'Customer.php';
 require_once 'PaymentProfile.php';
+require_once '../Payment/type/cash.php';
+require_once '../Payment/type/creditecard.php';
 
 # Classes & Objects
+$debite = new debite(120,200);
+var_dump($debite);
+
+$cash1 = (new Cash)->cash(1500, "MAD");
+var_dump($cash1);
 
 $transaction = new Transaction(4, 'test');
 echo $transaction?->getCustomer()?->getPaymentProfile()?->id ?? 'foo';
@@ -22,6 +29,4 @@ $myTransaction2 = (new Transaction(200, 'Transaction 2'))
 
 var_dump($myTransaction1->getAmount(), $myTransaction2->getAmount());
 */
-=======
 require_once '../Payment/Stripe/old_Transaction.php';
->>>>>>> refs/remotes/origin/main
