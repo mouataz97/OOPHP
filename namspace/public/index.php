0d3f27f1dc821cccdef131de23a2546ec96d2448
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-02-01 18:12:08 +0100

    namespaces 2

--- namspace/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '../Classes/Transaction.php';
-//require_once '../Classes/CustomerProfile.php';
-require_once '../Classes/DateTime.php';
-use Transaction;
-
-var_dump(new Transaction());
