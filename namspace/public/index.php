a6b5d8c0a95b23b768261b985153101cd1f1a650
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-02-01 14:23:00 +0100

    creating date class

--- namspace/public/index.php
+++ namspace/public/index.php
@@ -1,5 +1,8 @@
 <?php
 
 require_once '../Classes/Transaction.php';
+//require_once '../Classes/CustomerProfile.php';
+require_once '../Classes/DateTime.php';
+use Transaction;
 
 var_dump(new Transaction());
