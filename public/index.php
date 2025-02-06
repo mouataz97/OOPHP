7a5076efaad74082f14bd868e75003753f8f11dc
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-02-06 12:43:46 +0100

    generating composer-autoload -o

--- public/index.php
+++ public/index.php
@@ -2,10 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Payment\Paddle\Transaction;
 
-$paddleTransaction = new Transaction();
+//use App\Payment\Paddle\Transaction;
+
+//$paddleTransaction = new Transaction();
+//var_dump($paddleTransaction);
 
 $id = new \Ramsey\Uuid\UuidFactory();
 echo $id->uuid4();
-var_dump($paddleTransaction);
