40b90e180872738b1b72bf8542d51a3fd31e284a
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-02-06 13:59:03 +0100

    getting the value from the class

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\Payment\Paddle\Transaction;
 
 $transaction = new Transaction();
-echo $transaction::STATUS_PAID;
+//echo $transaction::STATUS_PAID;
+echo $transaction::class;
 
 //echo $id->uuid4();
