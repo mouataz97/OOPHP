d43ea7e67a4537a2b6426ec3e58b6700ee63a9ea
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-02-06 17:45:37 +0100

    properties and methods

--- App/Payment/Paddle/Transaction.php
+++ App/Payment/Paddle/Transaction.php
@@ -2,31 +2,47 @@
 
 declare(strict_types= 1);
 namespace App\Payment\Paddle;
-use App\Enums\Status;
+//use App\Enums\Status;
 
 class Transaction
 {
+    static public int $count = 0;
+    public function __construct(
+        public float $amount,
+        public string $description
+    ){
+        self::$count++;
+    }
+
+    public static function getCount(): int
+    {
+        return self::$count;
+    }
+
+    public function process(){
+        echo 'Processing paddle transaction...';
+    }
 
 //    private string $status;
 
-    public function __construct()
-    {
-        $this->setStatus(Status::PENDING);
-    }
-
-    public function setStatus(string $status): self
-    {
-        if(! isset(Status::ALL_STATUSES[$status])) {
-            throw new \InvalidArgumentException('invalid status');
-        }
-        $this->status = $status;
-
-        return $this;
-    }
-
-    // Add a getter method for the status
-//    public function getStatus(): string
+//    public function setStatus(string $status): self
 //    {
-//        return $this->status;
+//        if(! isset(Status::ALL_STATUSES[$status])) {
+//            throw new \InvalidArgumentException('invalid status');
+//        }
+//        $this->status = $status;
+//
+//        return $this;
+//    }
+//
+//    // Add a getter method for the status
+//
+////    public function getStatus(): string
+////    {
+////        return $this->status;
+////    }
+//    public function __construct()
+//    {
+//        $this->setStatus(Status::PENDING);
 //    }
 }
--- public/index.php
+++ public/index.php
@@ -2,11 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 use App\Payment\Paddle\Transaction;
-use App\Enums\Status;
+//use App\Enums\Status;
 
-$transaction = new Transaction();
-$transaction->setStatus(Status::PAID);
-var_dump($transaction); // You can inspect the entire object with var_dump
+$transaction = new Transaction(5,'tony alba');
+//$transaction->setStatus(Status::PAID);
+
+var_dump(Transaction::getCount()); // You can inspect the entire object with var_dump
+
 
 //echo $transaction->getStatus(); // This will print 'paid'
 
