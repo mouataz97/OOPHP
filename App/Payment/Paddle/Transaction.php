aac31ddebeb93594dd30349bb43bbdd3d01d03f0
El Mouataz Billah Fachatti <mouatazbillah97@gmail.com>
2025-02-07 19:39:10 +0100

    accessors for private variables

--- App/Payment/Paddle/Transaction.php
+++ App/Payment/Paddle/Transaction.php
@@ -2,50 +2,26 @@
 
 declare(strict_types= 1);
 namespace App\Payment\Paddle;
-//use App\Enums\Status;
+
 
 class Transaction
 {
-    static public int $count = 0;
-    public function __construct(
-        public float $amount,
-        public string $description
-    ){
-        self::$count++;
-    }
-
-    public function getCount(): int
+    private float $amount;
+    public function __construct(float $amount)
     {
-        array_map(function(){
-            var_dump($this->amount);
-        }, [1]);
-        return self::$count;
+        $this->amount =  $amount;
     }
 
-    public function process(){
-        echo 'Processe... ' . $this->amount . ' Transaction';
+    public function getAmount():float
+    {
+        return $this->amount ;
     }
-
-//    private string $status;
-
-//    public function setStatus(string $status): self
-//    {
-//        if(! isset(Status::ALL_STATUSES[$status])) {
-//            throw new \InvalidArgumentException('invalid status');
-//        }
-//        $this->status = $status;
-//
-//        return $this;
-//    }
-//
-//    // Add a getter method for the status
-//
-////    public function getStatus(): string
-////    {
-////        return $this->status;
-////    }
-//    public function __construct()
-//    {
-//        $this->setStatus(Status::PENDING);
-//    }
+    public function setAmount(float $amount): float
+    {
+        $this->amount = $amount;
+    }
+    public function process()
+    {
+        echo 'Processing $' . $this->amount . ' transaction';
+    }
 }
--- public/index.php
+++ public/index.php
@@ -1,20 +1,8 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-use App\DB;
 use App\Payment\Paddle\Transaction;
-//use App\Enums\Status;
 
-$transaction = new Transaction(5,'tony alba');
-//$db = new DB([]);
-$transaction->process();
-//var_dump($transaction->getCount());
-
-//$transaction->setStatus(Status::PAID);
-
-//var_dump(Transaction::getCount()); // You can inspect the entire object with var_dump
-
-//echo $transaction->getStatus(); // This will print 'paid'
-
-//$db = DB::getInstance([]);
+    $transaction = new Transaction(5);
+    $transaction->process();
 ?>
