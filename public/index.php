0c140a89b12b9cf3fc13b2a13757fbe74a0eee9c
mouataz <mouatazbillah97@gmail.com>
2025-02-10 15:21:54 +0100

    Interfaces & Polymorphism

--- public/index.php
+++ public/index.php
@@ -1,15 +1,5 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
-
-$fields = [
-    new App\Text('textField'),
-    new App\Checkbox('checkboxField'),
-    new App\Radio('radioField')
-];
-
-foreach ($fields as $field){
-    echo $field->render() . '<br>';
-}
+# Interfaces & Polymorphism
 ?>
