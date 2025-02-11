77a242c362e44dc34b95516b95a75d5f002fc261
mouataz <mouatazbillah97@gmail.com>
2025-02-11 15:44:26 +0100

    magic methods

--- public/index.php
+++ public/index.php
@@ -3,6 +3,3 @@
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 use App\implementation\DebtCollectionService;
 use App\implementation\Rocky;
-
-$Service = new DebtCollectionService();
-echo $Service->collectDebt(new Rocky()) . PHP_EOL ;
