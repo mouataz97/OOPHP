94a60cd0711a96f2491dd3f95f7e65528d62b564
mouataz <mouatazbillah97@gmail.com>
2025-02-11 23:57:30 +0100

    __invoke method

--- App/Inovice.php
+++ App/Inovice.php
@@ -3,48 +3,17 @@
 namespace App;
 # Magic Methods & How they work.
 
-class Inovice
+use AllowDynamicProperties;
+
+#[AllowDynamicProperties] class Inovice implements \Stringable
 {
-    public function process(float $amount, $discription)
+    public function __construct($name)
     {
-        var_dump($amount, $discription);
+        $this->name = $name;
     }
 
-    public function __call(string $name, array $arguments)
+    public function __toString(): string
     {
-        if(method_exists($this, $name)){
-            call_user_func_array([$this, $name], $arguments,);
-        }
+        return 'Hello World!';
     }
-//    public static function __callStatic(string $name, array $arguments)
-//    {
-//        var_dump('static', $name, $arguments);
-//    }
-//
-//
-//    public function __construct(public float $amount = 0,public array $data = []) // index cant work when i uncomment this method by calling in 2 lines
-//    {
-//    }
-//
-//    public function __get(string $name)
-//    {
-//        if(array_key_exists($name, $this->data)){
-//            return $this->data[$name];
-//        }
-//        return null;
-//    }
-//    public function __set(string $name, $value): void
-//    {
-//        $this->data[$name] = $value;
-//    }
-
-//    public function __isset(string $name): bool
-//    {
-//        return array_key_exists($name, $this->data);
-//    }
-//    public function __unset(string $name): void
-//    {
-//        unset($this->data[$name]);
-//    }
-//    */
 }
--- public/index.php
+++ public/index.php
@@ -3,15 +3,5 @@
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 use App\Inovice;
 
-$inovice = new Inovice();
-$inovice->process(13, 'test');
-
-//$inovice = new Inovice(25,'test');
-$inovice->amount = 25;
-
-
-//var_dump(isset($inovice->amount));
-
-//unset($inovice->amount);
-//var_dump($inovice->amount);
-
+$inovice = new Inovice('mouataz');
+echo $inovice->name;
