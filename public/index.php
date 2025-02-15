62c6a185f4381138cd8d76faf4926a7d79c5bcce
mouataz <mouatazbillah97@gmail.com>
2025-02-15 19:23:40 +0100

    comparing showing wrong value cus the instance is diffrent

--- public/index.php
+++ public/index.php
@@ -3,19 +3,16 @@
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 
 use \App\Invoice;
+use \App\CustomInvoice;
 # Variable Storage & Object Comparison
 
-$invoice1 = new Invoice(24, 'My invoice 1');
-$invoice2 = new Invoice(24, 'My invoice 1');
+$invoice1 = new Invoice(24, 'My invoice');
+$invoice2 = new CustomInvoice(24, 'My invoice');
 
-$invoice3 = $invoice1; // creating a pointer in $invoice3 of $invoice1
-$invoice3->amount = 123;
-echo 'dump $invoice1 and $invoice3';
-var_dump($invoice1, $invoice3);
-
-echo 'Invoice 1 == invoice 3' . PHP_EOL;
-var_dump($invoice1 == $invoice3);
+echo 'Invoice 1 == invoice 2' . PHP_EOL;
+var_dump($invoice1 == $invoice2);
 
 echo 'Invoice 1 === invoice 2' . PHP_EOL;
 var_dump($invoice1 === $invoice2);
 
+var_dump($invoice1, $invoice2);
