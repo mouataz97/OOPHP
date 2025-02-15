4104b11659dcc9835187561b0c110b4b82079650
mouataz <mouatazbillah97@gmail.com>
2025-02-15 17:24:21 +0100

    how to use anonymous classes

--- public/index.php
+++ public/index.php
@@ -1,20 +1,5 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
-use App\CoffeeShop\CoffeeMaker;
-use App\CoffeeShop\LatteMaker;
-use App\CoffeeShop\CappuccinoMaker;
-use App\CoffeeShop\AllInOneCoffeeMaker;
 
-$latteMaker = new LatteMaker();
-$latteMaker->makeCoffee();
-$latteMaker->makeLatte();
-
-$coffeeMaker = new CappuccinoMaker();
-$coffeeMaker->makeCoffee();
-$coffeeMaker->makeCappuccino();
-
-$allInOne = new AllInOneCoffeeMaker();
-$allInOne->makeCappuccino();
-$allInOne->makeCoffee();
-$allInOne->makeLatte();
+# Anonymous classes
