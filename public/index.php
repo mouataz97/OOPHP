d46571c100c6a445fddd8c8320e68de3c0138c04
mouataz <mouatazbillah97@gmail.com>
2025-02-15 18:07:14 +0100

    creating local class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,20 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
+use App\MyInterface;
 
 # Anonymous classes
 
+$obj = new class(1, 2, 3) implements MyInterface
+{
+    public function __construct(public int $x, public int $y, public int $z)
+    {
+    }
+};
+
+foo($obj);
+
+function foo(MyInterface $obj)
+{
+    var_dump($obj);
+}
