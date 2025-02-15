3afdc5981b141e718f6d12e8d58414a65c630802
mouataz <mouatazbillah97@gmail.com>
2025-02-15 18:36:57 +0100

    changing value of a pointer affect the original property value

--- public/index.php
+++ public/index.php
@@ -5,11 +5,17 @@ require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 use \App\Invoice;
 # Variable Storage & Object Comparison
 
-$invoice1 = new Invoice(1, 'My invoice 1');
-$invoice2 = new Invoice(true, 'My invoice 1');
+$invoice1 = new Invoice(24, 'My invoice 1');
+$invoice2 = new Invoice(24, 'My invoice 1');
 
-echo 'Invoice 1 == invoice 2' . PHP_EOL;
-var_dump($invoice1 == $invoice2);
+$invoice3 = $invoice1; // creating a pointer in $invoice3 of $invoice1
+$invoice3->amount = 123;
+echo 'dump $invoice1 and $invoice3';
+var_dump($invoice1, $invoice3);
+
+echo 'Invoice 1 == invoice 3' . PHP_EOL;
+var_dump($invoice1 == $invoice3);
 
 echo 'Invoice 1 === invoice 2' . PHP_EOL;
 var_dump($invoice1 === $invoice2);
+
