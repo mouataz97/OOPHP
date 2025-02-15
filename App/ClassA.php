b67c8ece6b7ea2f07b6e2ea07186b5b6d8c84426
mouataz <mouatazbillah97@gmail.com>
2025-02-15 18:11:38 +0100

    creating classA return anonymous  class bar

---
+++ App/ClassA.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class ClassA
+{
+
+    public function __construct(public int $x, public int $y)
+    {
+    }
+
+    public function foo(): string
+    {
+        return 'foo';
+    }
+    public function bar(): object
+    {
+        return new class {
+
+        };
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,20 +1,10 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
+use App\ClassA;
 use App\MyInterface;
 
 # Anonymous classes
 
-$obj = new class(1, 2, 3) implements MyInterface
-{
-    public function __construct(public int $x, public int $y, public int $z)
-    {
-    }
-};
-
-foo($obj);
-
-function foo(MyInterface $obj)
-{
-    var_dump($obj);
-}
+$obj = new ClassA(1, 2);
+var_dump($obj->bar());
