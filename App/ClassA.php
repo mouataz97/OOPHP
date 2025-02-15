cb0f1f3689ed7f058abdfd4d99c14a4595556b21
mouataz <mouatazbillah97@gmail.com>
2025-02-15 18:19:36 +0100

    other option to access the class by using  property

--- App/ClassA.php
+++ App/ClassA.php
@@ -15,11 +15,10 @@ class ClassA
     }
     public function bar(): object
     {
-        return new class extends ClassA {
+        return new class($this->x, $this->y) {
             public function __construct(public int $x, public int $y)
             {
-                parent::__construct($x, $y);
-                echo $this->foo();
+                var_dump($x, $y);
             }
         };
     }
