68c1805cfafca6eb22d3950d82ac3fddfca422a4
mouataz <mouatazbillah97@gmail.com>
2025-02-17 16:38:15 +0100

    serializing each property can have diffrent return

--- public/index.php
+++ public/index.php
@@ -6,3 +6,10 @@ use \App\Invoice;
 
 # Object & Serialize Magic Methods
 
+$invoice = new Invoice();
+echo serialize(true). PHP_EOL;
+echo serialize(1). PHP_EOL;
+echo serialize(2.5). PHP_EOL;
+echo serialize('hello'). PHP_EOL;
+echo serialize([1,2]). PHP_EOL;
+echo serialize(['a'=>1,'b'=>2]). PHP_EOL;
