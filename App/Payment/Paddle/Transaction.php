250f8988ccde4c06689de2414ec7c5275bf05baa
mouataz <mouatazbillah97@gmail.com>
2025-02-17 15:41:19 +0100

    Object cloning & and clone Magic Methods

--- App/Payment/Paddle/Transaction.php
+++ App/Payment/Paddle/Transaction.php
@@ -5,18 +5,8 @@ namespace App\Payment\Paddle;
 
 use App\Customer;
 
-/**
- * @method static int foo(string $x)
- */
+
 class Transaction
 {
-    public function __call(string $name, array $arguments)
-    {
-        // TODO: Implement __call() method.
-    }
 
-    public static function __callStatic(string $name, array $arguments)
-    {
-        // TODO: Implement __callStatic() method.
-    }
 }
