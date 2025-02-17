8ebea9dd23871cb3e16347e81628d1552362fbb2
mouataz <mouatazbillah97@gmail.com>
2025-02-17 15:56:52 +0100

    Object & Serialize Magic Methods

--- public/index.php
+++ public/index.php
@@ -4,11 +4,5 @@ require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 
 use \App\Invoice;
 
-# Object cloning & Magic Methods
-
-$invoice1 = new Invoice();
-
-$invoice2 = clone $invoice1;
-
-var_dump($invoice1, $invoice2, $invoice1===$invoice2);
+# Object & Serialize Magic Methods
 
