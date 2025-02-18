713d852b99709b25bbacf95e3c07fa4e2581a23f
mouataz <mouatazbillah97@gmail.com>
2025-02-18 23:43:56 +0100

    using error handling porfissionally

--- public/index.php
+++ public/index.php
@@ -7,11 +7,28 @@ use App\Customer;
 
 # Exceptions: Try-Catch || OOP Error Handling
 
-$invoice = new Invoice(new Customer());
-try{
-    $invoice->process(25);
-} catch (\Exception $e){
-    echo $e->getMessage() . PHP_EOL;
-} finally {
-    echo "Finally block". PHP_EOL;
+function foo(){
+    echo 'foo' . PHP_EOL;
+    return false;
+}
+
+
+var_dump(process());
+function process()
+{
+    $invoice = new Invoice(new Customer(['foo','bar']));
+    try{
+        $invoice->process(-25);
+
+        return true;
+    } catch (\Exception $e){
+        echo $e->getMessage() . PHP_EOL;
+
+        return foo();
+    } finally {
+        echo "Finally block". PHP_EOL;
+
+        return -1;
+    }
+
 }
