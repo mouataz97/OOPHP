77778f86ac1e424db74069994672d495b7dd847a
mouataz <mouatazbillah97@gmail.com>
2025-02-19 16:52:18 +0100

    using settimezone, getName and format

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 // DateTime Object
 # https://www.php.net/manual/en/book.datetime.php
 
-$dateTime = new DateTime('Tomorrow 3:35pm');
-echo $dateTime->format('m/d/y g:i A').PHP_EOL;
+$dateTime = new DateTime('Now');
+echo $dateTime->getTimezone()->getName() . ' ' . $dateTime->format('m/d/y g:i A').PHP_EOL;
 
 $dateTime->setTimezone(new DateTimeZone('Europe/Amsterdam'));
-echo $dateTime->format('m/d/y g:i A').PHP_EOL;
+echo $dateTime->getTimezone()->getName() . ' ' . $dateTime->format('m/d/y g:i A').PHP_EOL;
