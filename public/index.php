ce5999f36750154bc9dc397d5af73c68a3bca5d5
mouataz <mouatazbillah97@gmail.com>
2025-02-19 15:43:11 +0100

    using Throwable to be able to catch the error

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@ use App\Customer;
 
 # Exceptions: Try-Catch || OOP Error Handling
 
-set_exception_handler(function(\Exception $e){
+set_exception_handler(function(\Throwable $e){
     var_dump($e->getMessage());
 });
 
 try{
     array_rand([],1);
-}catch (\ValueError $e){
+}catch (\Throwable $e){
     echo $e->getMessage() . PHP_EOL;
 }
 
