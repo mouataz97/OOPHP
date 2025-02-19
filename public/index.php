4b8f949e01865406e05ee7ac38915ace86df098f
mouataz <mouatazbillah97@gmail.com>
2025-02-19 16:06:11 +0100

    time zone methods

--- public/index.php
+++ public/index.php
@@ -2,7 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 // DateTime Object
+# https://www.php.net/manual/en/book.datetime.php
 
-$dateTime = new DateTime();
+$dateTime = new DateTime('Tomorrow 3:35pm');
+$dateTime2 = new DateTime('05/12/2025 12:00am', new DateTimeZone('Africa/Casablanca'));
 
 var_dump($dateTime);
+echo PHP_EOL;
+var_dump($dateTime2);
