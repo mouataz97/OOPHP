3a9d394502868072a088dcf022020c23103d9545
mouataz <mouatazbillah97@gmail.com>
2025-02-20 19:31:33 +0100

    formating date

--- public/index.php
+++ public/index.php
@@ -4,9 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 // DateTime Object
 # https://www.php.net/manual/en/book.datetime.php
 
-$dateTime = new DateTime('Now');
-echo $dateTime->getTimezone()->getName() . ' ' . $dateTime->format('m/d/y g:i A').PHP_EOL;
+$date = new DateTime('15/05/2015 19:20PM');
+$dateTime = new DateTime(str_replace('/', '-', $date));
 
-$dateTime->setTimezone(new DateTimeZone('Europe/Amsterdam'));
-$dateTime->se0tDate(2021,3, 14)->setTime(16, 56);
-echo $dateTime->getTimezone()->getName() . ' ' . $dateTime->format('m/d/y g:i A').PHP_EOL;
+var_dump($dateTime);
