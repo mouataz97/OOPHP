fd279b0856d4420d368c18b323954d43c634fb1d
mouataz <mouatazbillah97@gmail.com>
2025-02-20 19:56:52 +0100

    stating the diffrence between 2 variables with diffrent calanders specificly by times

--- public/index.php
+++ public/index.php
@@ -4,11 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 // DateTime Object
 # https://www.php.net/manual/en/book.datetime.php
 
-$date1 = new DateTime('05/05/2015 9:15PM');
-$date2 = new DateTime('05/05/2015 9:15AM');
+$date1 = new DateTime('04/05/2015 9:15 PM');
+$date2 = new DateTime('05/05/2015 9:50 AM');
 
-var_dump($date1 < $date2);
-var_dump($date1 > $date2);
-var_dump($date1 === $date2);
-var_dump($date1 <=> $date2);
+var_dump($date1->diff($date2));
 
