76f6f3c2697628cc735eb9b73324bb3b15e6db0d
mouataz <mouatazbillah97@gmail.com>
2025-02-22 20:57:55 +0100

    use classs invoiceCollection implement the iterator

--- App/InvoiceCollection.php
+++ App/InvoiceCollection.php
@@ -3,20 +3,7 @@
 namespace App;
 use Traversable;
 
-class InvoiceCollection implements \IteratorAggregate
+class InvoiceCollection extends Collection
 {
 
-    /**
-     * @Invoice className
-     * @property int
-     */
-    private int $key;
-    public function __construct(public array $invoices)
-    {
-    }
-
-    public function getIterator(): Traversable
-    {
-        return new \ArrayIterator($this->invoices);
-    }
 }
--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 
 use App\Invoice;
 use App\InvoiceCollection;
+use App\Collection;
 
 foreach (new Invoice(35) as $key => $value) {
     echo $key . ' = ' . $value . PHP_EOL;
@@ -15,4 +16,12 @@ $invoiceCollection = new InvoiceCollection([new Invoice(14), new Invoice(25), ne
 
 foreach ($invoiceCollection as $invoice){
     echo $invoice->id . ' - ' . $invoice->amount . PHP_EOL;
+}
+
+foo($invoiceCollection);
+
+function foo(Collection|array $iterable){
+    foreach($iterable as $i => $item){
+        echo $i . PHP_EOL;
+    }
 }
