aecd921399ac8dd26f9c92987675321d4283662d
mouataz <mouatazbillah97@gmail.com>
2025-02-22 16:51:08 +0100

    we cant access Invoice properties

---
+++ App/InvoiceCollection.php
@@ -0,0 +1,12 @@
+<?php
+
+class InvoiceCollection
+{
+
+    /**
+     * @Invoice className
+     */
+    public function __construct(public array $invoices)
+    {
+    }
+}
--- public/index.php
+++ public/index.php
@@ -9,3 +9,8 @@ foreach (new Invoice(35) as $key => $value) {
     echo $key . ' = ' . $value . PHP_EOL;
 }
 
+$invoiceCollection = new InvoiceCollection([new Invoice(14), new Invoice(25), new Invoice(50)]);
+
+foreach ($invoiceCollection as $invoice){
+    echo $invoice->id . ' - ' . $invoice->amount . PHP_EOL;
+}
