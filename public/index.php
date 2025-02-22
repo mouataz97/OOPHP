a7c3174d923fceff2adc8e6c8b5812d462a888b3
mouataz <mouatazbillah97@gmail.com>
2025-02-22 16:27:07 +0100

    Iterators & Iterable Type

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
-// DateTime Object
-# https://www.php.net/manual/en/book.datetime.php
-
-$date1 = new DateTime('04/05/2015 9:15 PM');
-$date2 = new DateTime('05/05/2015 9:50 AM');
-
-var_dump($date1->diff($date2));
+// Iterators & Iterable Type
 
