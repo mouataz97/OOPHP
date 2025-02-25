578f78e37fc34824eda7550968bf70b4c73de818
mouataz <mouatazbillah97@gmail.com>
2025-02-25 17:21:39 +0000

    using super global get and post

--- App/Classes/Home.php
+++ App/Classes/Home.php
@@ -4,7 +4,16 @@ namespace App\Classes;
 
 class Home
 {
-    public static function index(){
-        return 'Home';
+    public static function index(): string
+    {
+        echo '<pre>';
+        var_dump($_GET);
+        echo '<pre>';
+
+        echo '<pre>';
+        var_dump($_POST);
+        echo '<pre>';
+
+        return '<form action="/foo=bar&amount=149" method="post"><label>Amount<input type="text"></label></form>';
     }
 }
