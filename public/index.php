06c091ff7d417572a002488b814437416c372638
mouataz <mouatazbillah97@gmail.com>
2025-02-25 17:10:27 +0000

    Get & Post Superglobals based onthe route

--- public/index.php
+++ public/index.php
@@ -2,3 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 # Get & Post Superglobals
+
+$router = new App\Router();
+
+$router
+    ->register('/', [App\Classes\Home::class, 'index'])
+    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
+
+// Check if REQUEST_URI exists to avoid error in CLI or other non-web contexts
+$requestURI = $_SERVER['REQUEST_URI'] ?? '/';  // Default to '/' if REQUEST_URI is not set
