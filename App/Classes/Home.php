2db6a3acf3786290ebd6c42adb33c55b2c0693d7
mouataz <mouatazbillah97@gmail.com>
2025-02-26 23:51:44 +0000

    staging Sessions

--- App/Classes/Home.php
+++ App/Classes/Home.php
@@ -6,18 +6,6 @@ class Home
 {
     public function index(): string
     {
-        echo '<pre>';
-        var_dump($_REQUEST);
-        echo '<pre>';
-
-        echo '<pre>';
-        var_dump($_GET);
-        echo '<pre>';
-
-        echo '<pre>';
-        var_dump($_POST);
-        echo '<pre>';
-
-        return '<form action="/foo=bar&amount=149" method="post"><label>Amount<input type="text"></label></form>';
+        return 'Home';
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,19 +1,20 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
-# Get & Post Superglobals
-use App\Classes\Home;
-use App\Classes\Invoice;
-use App\Classes\Router;
-
+# Sessions & Cookies - Output Buffering
 
 $router = new \App\Classes\Router();
 
 $router
-    ->get('/', [Home::class, 'index'])
-    ->get('/invoices', [Invoice::class, 'index'])
-    ->get('/invoices/create', [Invoice::class, 'create'])
-    ->post('/invoices/create', [Invoice::class, 'store']);
+    ->get('/', [App\Classes\Home::class, 'index'])
+    ->get('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->get('/invoices/create', [App\Classes\Invoice::class, 'create'])
+    ->post('/invoices/create', [App\Classes\Invoice::class, 'store']);
 
 
-echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));
+echo $router->resolve(
+    $_SERVER['REQUEST_URI'],
+    strtolower($_SERVER['REQUEST_METHOD'])
+);
+
+session_start();
