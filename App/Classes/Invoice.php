60a80192f87ab3fc65e5f3552d5ce91aba41c25b
El Mouataz Billah <mouatazbillah97@gmail.com>
2025-02-26 19:45:39 +0000

    using post to post a request method from server super globbal

--- App/Classes/Invoice.php
+++ App/Classes/Invoice.php
@@ -10,6 +10,12 @@ class Invoice
     }
     public function create(): string
     {
-        return 'Create Invoice';
+        return '<form action="/Invoice/create" method="post"><label>Amount</label><input type="text">';
+    }
+
+    public function store()
+    {
+        $amount = $_POST['amount'];
+        var_dump($amount);
     }
 }
--- public/index.php
+++ public/index.php
@@ -2,13 +2,18 @@
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 # Get & Post Superglobals
+use App\Classes\Home;
+use App\Classes\Invoice;
+use App\Classes\Router;
+
 
 $router = new \App\Classes\Router();
 
 $router
-    ->register('/', [App\Classes\Home::class, 'index'])
-    ->register('/invoices', [App\Classes\Invoice::class, 'index'])
-    ->register('/invoices/create', [App\Classes\Invoice::class, 'create']);
+    ->get('/', [Home::class, 'index'])
+    ->get('/invoices', [Invoice::class, 'index'])
+    ->get('/invoices/create', [Invoice::class, 'create'])
+    ->post('/invoices/create', [Invoice::class, 'create']);
 
-// Check if REQUEST_URI exists to avoid error in CLI or other non-web contexts
-$requestURI = $_SERVER['REQUEST_URI'] ?? '/';  // Default to '/' if REQUEST_URI is not set
+
+echo $router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
