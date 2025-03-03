76279901be68d3fcb5a445e60a5004a153218eff
El Mouataz Billah <mouatazbillah97@gmail.com>
2025-03-03 18:14:20 +0000

    MVC

--- public/index.php
+++ public/index.php
@@ -2,23 +2,4 @@
 
 require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 
-session_start();
-define('STORAGE_PATH', __DIR__ . '/../storage');
-# Get & Post Superglobals
-
-use App\Classes\Home;
-use App\Classes\Invoice;
-
-$router = new \App\Classes\Router();
-session_start();
-$router
-    ->get('/', [Home::class, 'index'])
-    ->post('/Upload', [Home::class, 'Upload'])
-    ->get('/invoices', [Invoice::class, 'index'])
-    ->get('/invoices/create', [Invoice::class, 'create'])
-    ->post('/invoices/create', [Invoice::class, 'store']);
-
-echo $router->resolve(
-    $_SERVER['REQUEST_URI'],
-    strtolower($_SERVER['REQUEST_METHOD'])
-);
+# MVC
