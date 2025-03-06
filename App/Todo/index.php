da4499a621d24a6fad5b71e9099c5fa6a1dcd215
El Mouataz Billah <mouatazbillah97@gmail.com>
2025-03-06 19:39:16 +0000

    creating form with file

---
+++ App/Todo/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<?php
+define('STORAGE', __DIR__ . '/storage.txt');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['todo'])) {
+    file_put_contents(STORAGE, PHP_EOL, FILE_APPEND);
+    file_put_contents(STORAGE, $_POST['todo'], FILE_APPEND);
+}
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+        <label for="">insert a message</label>
+        <input type="text" name="todo">
+        <button type="submit">Add</button>
+    </form>
+    <div>
+        <?php
+    function renderTodos(array $todos): void
+    {
+        echo "<ul>";
+        foreach ($todos as $key => $todo) {
+            echo "<li style='margin-top: 10px'>";
+            echo '<form action="index.php" method="POST">';
+            echo '<input type="hidden" name="delete" value="' . $key . '">';
+            echo '<button type="submit"> X </button>';
+            echo " $todo";
+            echo "</form>";
+            echo "</li>";
+        }
+        echo "</ul>";
+    }
+?>
+    </div>
+</body>
+</html>
