4dc97fe04b1143885cbdf640ad271c3c3a57df8a
mouataz <mouatazbillah97@gmail.com>
2025-04-25 18:44:32 +0100

    creating some classes in main

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';  // Composer autoload
-// DateTime Object
-# https://www.php.net/manual/en/book.datetime.php
+require_once __DIR__ . '/../vendor/autoload.php';  // Composer autoload
 
-$dateTime = new DateTime('Now');
-echo $dateTime->getTimezone()->getName() . ' ' . $dateTime->format('m/d/y g:i A').PHP_EOL;
+session_start();  // Start the session
 
-$dateTime->setTimezone(new DateTimeZone('Europe/Amsterdam'));
-$dateTime->se0tDate(2021,3, 14)->setTime(16, 56);
-echo $dateTime->getTimezone()->getName() . ' ' . $dateTime->format('m/d/y g:i A').PHP_EOL;
+$router = new App/Router();
+$router
+    ->get('/', [App\Classes\HomeController::class, 'index'])
+    ->post('/upload', [App\Classes\Home::class, 'upload'])
+    ->get('/invoices', [App\Classes\Invoice::class, 'index'])
+    ->get('/invoices/create', [App\Classes\Invoice::class, 'create'])
+    ->post('/invoices/create', [App\Classes\Invoice::class, 'create']);
+
+echo $router->resolve(
+    $_SERVER['REQUEST_URI'],
+    strtolower($_SERVER['REQUEST_METHOD'])
+);
