0376a3202115ea4e38545eda1911f46025a9c15b
unknown <mouatazbillah97@gmail.com>
2025-06-26 16:45:06 +0100

    creating a user for transactions

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -5,44 +5,68 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\View;
+use \PDO;
 
 class HomeController
 {
     public function index(): View
     {
         try{
-            $db = new \PDO('mysql:host=127.0.0.1;dbname=my_db', 'root', 'root', [
+            $db = new PDO('mysql:host=my_db;dbname=my_db', 'root', 'root', [
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ
             ]);
-
-            $email = 'jhones@doe.com';
-            $name = 'Jhones Doe';
-            $is_active = 1;
-            $createAT = date('Y-m-d H:i:s', strtotime('06/21/2025 2:12 PM'));
-            $query = 'INSERT INTO users (email, full_name, is_active, created_at) 
-                      VALUES (:email, :name, :active, :date)';
-
-            $stmt = $db->prepare($query);
-
-            $stmt->bindValue(':name', $name);
-            $stmt->bindValue(':email', $email);
-            $stmt->bindParam(':active', $is_active, \PDO::PARAM_BOOL);
-            $stmt->bindValue(':date', $createAT);
-            $is_active = 0; // Example of changing the value before execution
-            $stmt->execute();
-
-            $id = (int) $db->lastInsertId();
-            $user = $db->query("SELECT * FROM users WHERE id = $id")->fetch();
-
-            echo '<pre>' ;
-            var_dump($user);
-            echo '</pre>';
-            
-        }catch(\PDOException $e){
-            var_dump($e->getCode())
-            throw new \PDOException($e->getMessage(), $e->getCode());
+        } catch (\PDOException $e) {
+            throw new \PDOException( $e->getMessage(), (int) $e->getCode()); 
         }
 
-        return View::make('index');
+            $email = 'john@doe.com';
+            $name = 'John Doe';
+            $amount = 25;
+
+        try {
+        $db->beginTransaction();
+
+            $newUserStmt = $db->prepare(
+                'INSERT INTO users (email, full_name, is_active, Cceated_at) 
+                VALUES (?, ?, 1, NOW())'
+            ); 
+            
+            $newInvoiceStmt = $db->prepare(
+                'INSERT INTO invoices (amount, user_id) 
+                VALUES (?,?)'
+            ); 
+        }catch (\Throwable $e) {
+            if($db->inTransaction()) {
+                // Rollback the transaction if it was started
+            $db->rollBack();
+        }
+
+            $fetchUserStmt = $db->prepare(
+                'SELECT id FROM users WHERE email = ?'
+            );
+
+            $fetchUserStmt->execute([$email]);
+            $user = $fetchUserStmt->fetch();
+
+            if ($user) {
+                throw new \Exception('User already exists');
+        }
+
+            $newUserStmt->execute([$email, $name]);
+
+            $userId = $db->lastInsertId();
+
+            $newInvoiceStmt->execute([$amount, $userId]);
+
+            $db->commit();
+
+            $fetchInvoicesStmt = $db->prepare(
+                'SELECT invoices.id as invoice_id, user_id, full_name
+                FROM invoices
+                Inner JOIN users ON users_id = user.id
+                WHERE email = ?'
+            );
+            
+
     }
 }
